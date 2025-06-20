d1db4e5a62180aa7585f0217c24b9d993f6283bc
Ayush Trivedi <ayushtrivedi890@gmail.com>
2025-06-20 09:56:59 +0530

    code completed

---
+++ app/main.py
@@ -0,0 +1,5 @@
+def hello():
+    return "Hello from DevSecOps"
+
+if __name__ == "__main__":
+    print(hello())
---
+++ tests/test_main.py
@@ -0,0 +1,4 @@
+from app.main import hello
+
+def test_hello():
+    assert hello() == "Hello from DevSecOps"
